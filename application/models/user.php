04c42f9e7cec02a92f083b087e38a42f52e8cf6d
Serkan Ilyaz <serkanofficial@abv.bg>
2022-06-30 11:33:19 +0300

    Further work on project structure

---
+++ application/models/user.php
@@ -0,0 +1,40 @@
+<?php
+
+class User 
+{
+    private $username, $email, $password;
+
+    public function __construct() {}
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+}
