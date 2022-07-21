f444cea7cdc33d70b9008a4374ab6dc6bb07e673
Serkan Ilyaz <serkanofficial@abv.bg>
2022-07-21 19:50:49 +0300

    Make forms in the header links

--- application/views/Common/header.php
+++ application/views/Common/header.php
@@ -1,20 +1,16 @@
 <?php $isLoggedIn = isset($data['loggedUser']) ?>
 <header class="headerCust" style="<?php if (!$isLoggedIn) echo 'justify-content: center' ?>">
-    <a href="/home"><img src="/img/logo-transparent.png" alt="error" width="400px" height="60px"></a>
+    <a href="/home"><img src="/img/logo-transparent.png" alt="fitmissive" width="400px" height="60px"></a>
     <?php if ($isLoggedIn) { ?>
         <div class="menu">
             <div class="items">
                 <form action="/search">
-                    <input type="text" class="menu-item" class name="search" placeholder="Search users" style="margin: 8px"
-                    value="<?php if (isset($data['keyword'])) echo $data['keyword'] ?>">
+                    <input type="text" class="search-field" name="search" placeholder="Search users" style="margin: 8px"
+                        value="<?php if (isset($data['keyword'])) echo $data['keyword'] ?>">
                     <input type="submit" class="menu-item" value="Search" style="margin-right:30px">
                 </form>
-                <form action="/profile">
-                    <input type="submit" class="menu-item" value="My profile">
-                </form>
-                <form action="/home/logout">
-                    <input type="submit" class="menu-item" style="margin: 10px" value="Log out">
-                </form>
+                <a href="/profile" class="menu-item">My profile</a>
+                <a href="/home/logout" class="menu-item" style="margin: 10px">Sign out</a>
             </div>
         </div>
     <?php } ?>
