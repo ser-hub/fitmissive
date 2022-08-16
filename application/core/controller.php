794024768f94d93cd2bc60132bf749636aea5fba
Serkan Ilyaz <serkanofficial@abv.bg>
2022-08-16 12:42:20 +0300

    Update controller

--- application/core/controller.php
+++ application/core/controller.php
@@ -3,24 +3,29 @@
 namespace Application\Core;
 
 use Application\Utilities\Redirect;
-use Application\Services\{UserService, InfoService};
+use Application\Services\{UserService, InfoService, ChatService};
 
 class Controller
 {
     protected $loggedUser;
     protected $userService;
+    protected $chatService;
     private $infoService = null;
+    private $loggedUsername;
 
     public function __construct()
     {
         $this->userService = UserService::getInstance();
         $this->infoService = InfoService::getInstance();
+        $this->chatService = ChatService::getInstance();
 
         if (!$this->userService->isUserLoggedIn()) {
             Redirect::to('/index');
         }
 
-        $this->loggedUser = $this->userService->getLoggedUser()->user_id;
+        $this->loggedUser = $this->userService->getLoggedUser();
+        $this->loggedUsername = $this->loggedUser->username;
+        $this->loggedUser = $this->loggedUser->user_id;
     }
 
     protected function model($model)
@@ -33,11 +38,19 @@ class Controller
     {
         if ($this->loggedUser) {
             $data['loggedUser'] = $this->loggedUser;
+            $data['loggedUsername'] = $this->loggedUsername;
+            $data['menu'] = [
+                'Messenger' => '/messenger', 
+                'My profile' => '/profile/' . $this->loggedUsername,
+                'Sign out' => '/home/logout'
+            ];
+            $data['unseenMessages'] = $this->chatService->unseenMessages($this->loggedUser);
         }
 
         if ($this->infoService == null) {
             $this->infoService = InfoService::getInstance();
         }
+        
         $data['info'] = [];
         $data['info'] = $this->infoService->getAllInfo();
         require_once 'Application/Views/' . $view . '.php';
