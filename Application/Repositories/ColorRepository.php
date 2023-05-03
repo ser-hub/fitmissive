ba0a916d581f1026f6883f066430ee2c9ac0a4cb
Serkan Ilyaz <serkanofficial@abv.bg>
2023-05-03 16:48:43 +0300

    Add ColorRepository

---
+++ Application/Repositories/ColorRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Application\Repositories;
+
+use Application\Database\DB;
+
+class ColorRepository
+{
+    private static $instance = null;
+    private $db;
+    private const COLORS_TABLE = "colors";
+
+    private function __construct()
+    {
+        $this->db = DB::getInstance();
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getAll()
+    {
+        $data = $this->db->query('SELECT * FROM ' . self::COLORS_TABLE);
+
+        if ($data->count()) {
+            return $data->results();
+        }
+    }
+
+    public function getColorByHex($value)
+    {
+        $data = $this->db->get(self::COLORS_TABLE, ['color_hex', '=', $value]);
+
+        if ($data->count()) {
+            return $data->first();
+        }
+        return false;
+    }
+}
